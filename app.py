daf5e2b48a46e0a856ce1a943fa6db8ed673850d
Sarichand <143803994+srichimrani@users.noreply.github.com>
2026-05-16 00:22:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def greet(name):
-    return "Hello " + name + " !!"
+    return "Hello updated app"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch()
